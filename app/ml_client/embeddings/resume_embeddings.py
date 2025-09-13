673474967130631689b4d27bb175e43ae9bfb0ae
JahleelT <jlt9287@nyu.edu>
2025-09-13 14:56:09 -0400

    updated vectorization logic

--- app/ml_client/embeddings/resume_embeddings.py
+++ app/ml_client/embeddings/resume_embeddings.py
@@ -1,15 +1,25 @@
 from langchain.embeddings import HuggingFaceBgeEmbeddings
 
-def embed_chunks(chunks: list[Document]):
-  embeddings = HuggingFaceBgeEmbeddings(model_name="all-MiniLM-L6-v2")
 
+embeddings = HuggingFaceBgeEmbeddings(model_name="all-MiniLM-L6-v2")
+
+def embed_chunks(chunks: list[Document]):
   texts = [chunk.page_content for chunk in chunks]
   vectors = embeddings.embed_documents(texts)
 
-  vector_data = [
-    {"vector": vec, "metadata": chunk.metadata}
-    for vec, chunk in zip(vectors, chunks)
-  ]
+  vector_data = []
+  
+  for (i, (vec, chunk)) in enumerate(zip(vectors, chunks)):
+    resume_id = chunk.metadata.get("resume_id", "unknown")
+    vector_id = f"{resume_id}_chunk{i}"
+
+    vector_data.append(
+      {
+        "id": vector_id,
+        "values": vec,
+        "metadata": chunk.metadata
+      }
+    )
 
   return vector_data
 
