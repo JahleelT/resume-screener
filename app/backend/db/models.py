b0ed1ff2a8c040ff89a5b2c9b81684741d40bba6
JahleelT <jlt9287@nyu.edu>
2025-09-19 04:12:56 -0400

    updated an attribute for Resume

--- app/backend/db/models.py
+++ app/backend/db/models.py
@@ -18,7 +18,7 @@ class Resume(Base):
   __tablename__ = "resumes"
 
   id = Column(Integer, primary_key = True, index = True)
-  user_id = Column(Integer, ForeignKey("users.id"))
+  user_id = Column(Integer, ForeignKey("users.id"), nullable = False)
   text = Column(Text)
   created_at = Column(DateTime(timezone=True), server_default=func.now())
 
