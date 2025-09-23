66ab47f13ff68d9f2574bb8c3bfeee35c5abcaed
JahleelT <jlt9287@nyu.edu>
2025-09-22 23:20:25 -0400

    updated flask factory to remove mongo, init indexes, setup jwt

--- app/app.py
+++ app/app.py
@@ -1,27 +1,28 @@
 import os
 from flask import Flask
 from dotenv import load_dotenv
-from flask.json.provider import DefaultJSONProvider
-from bson import ObjectId
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
-from login import login_manager
+from backend.utils.pinecone_init import begin_index
 
 load_dotenv()
 
-class MongoJSONProvider(DefaultJSONProvider):
-    def default(self, o):
-        if isinstance(o, ObjectId):
-            return str(o)
-        return super().default(o)
+
+jwt = JWTManager()
 
 def create_app():
   app = Flask(__name__)
   app.secret_key = os.getenv("SECRET_KEY", "dev-change-me")
 
-  app.json_provider_class = MongoJSONProvider
+  app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "super-secret")
+  jwt.init_app(app)
 
-  login_manager.init_app(app)
-  login_manager.login_view = "frontend.login"
+  app.config["RESUME_INDEX"] = begin_index("resumes")
+  app.config["JD_INDEX"] = begin_index("job_descriptions")
+
+  CORS(app, resources={r"/api/*": {"origins": "*"}})
+
 
   from frontend.routes import frontend_bp
   from backend.app import routes_bp
