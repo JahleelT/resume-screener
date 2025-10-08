baeb5b5e935044038f94b6db0473fc6184d19a5a
JahleelT <jlt9287@nyu.edu>
2025-10-08 01:52:08 -0400

    finished button prototype, worked on login.tsx

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -2,19 +2,22 @@ import React from 'react';
 import "tailwindcss"; 
 
 interface ButtonProps {
-  childrem: React.ReactNode;
+  children: React.ReactNode;
   color?: string;
   textColor ?: string;
   onClick ?: (() => void);
-  disabled ?: boolean;
 }
 
 
-export const Button: React.FC<ButtonProps> = ( children) => {
+export const Button: React.FC<ButtonProps> = ({ children, color="bg-blue-500", textColor="text-black", onClick}) => {
 
   return (
-    <button></button>
-  )
+    <button
+    onClick={onClick}
+    className={`px-4 py-2 ${color} border border-black ${textColor}, font-semibold rounded-lg shadow-md transition mt-2 w-full`}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useState, } from 'react';
 
 export const Login: React.FC = () => {
-
+  const [msg, setMsg] = useState("");
   return (
     <>
-    
+      <h1>Log In</h1>
+      <ul className="list-none p-0 mb-4">
+        <li className="p-2 rounded-b mb-2 font-medium">
+          { msg }
+        </li>
+      </ul>
     </>
   )
 }
