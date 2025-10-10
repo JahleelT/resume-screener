26b10a6fc80815b50375a4b31a24603b9ef027d1
JahleelT <jlt9287@nyu.edu>
2025-10-09 23:14:14 -0400

    basic logic for signup done

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
+import "tailwindcss";
 
 export const SignUp: React.FC = () => {
+  const [msg, setMsg] = useState("");
 
   return (
     <>
-    
+    <h1>Sign In</h1>
+    <ul className='list-none p-0 mb-4'>
+      <li className='p-2 rounded-b mb-2 font-medium'>
+        { msg }
+      </li>
+    </ul>
     </>
   )
 }
