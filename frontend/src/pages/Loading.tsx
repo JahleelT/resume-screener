77c397c333a41d92b4b45b196f9f61c4806d9fe2
JahleelT <jlt9287@nyu.edu>
2026-08-26 21:26:12 -0400

    fixed type error in models.py

--- frontend/src/pages/Loading.tsx
+++ frontend/src/pages/Loading.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useNavigate, useParams}
 
 export const Loading: React.FC = () => {
-
+  const [results, setResults] = useState(['']) 
   return (
     <>
+      
       <h1>Hang tight- your resume is being analyzed...</h1>
 
     </>
